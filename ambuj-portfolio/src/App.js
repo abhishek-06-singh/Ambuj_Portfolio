717dc1c152bed6d7506f876daa63a95030bda73f
Abhishek singh chauhan <avi7000201383@gmail.com>
2023-11-27 23:55:02 +0530

    added hero section

--- ambuj-portfolio/src/App.js
+++ ambuj-portfolio/src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
+import Hero from "./components/navbar/hero/Hero";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Hero />
     </div>
   );
 }
