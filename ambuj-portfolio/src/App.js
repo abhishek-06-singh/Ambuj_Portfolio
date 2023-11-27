ff4cbd4ddfc65f3df4a6d688698aeaedae631abe
Abhishek singh chauhan <avi7000201383@gmail.com>
2023-11-28 00:22:00 +0530

    added about me section

--- ambuj-portfolio/src/App.js
+++ ambuj-portfolio/src/App.js
@@ -2,12 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/navbar/hero/Hero";
+import Feature from "./components/feature/Feature";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
+      <Feature />
     </div>
   );
 }
